428bdf48b3cbfb983162e8f96f7b18f34b550445
Shivani Pandey <misrashivani@yahoo.com>
2026-03-10 18:32:26 +0800

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { About } from './components/about/about';
+import { Clients } from './components/clients/clients';
+import { Pricing } from './components/pricing/pricing';
+import { Testimonials } from './components/testimonials/testimonials';
+import { Gallery } from './components/gallery/gallery';
+import { Nav } from './components/nav/nav';
+import { Services } from './components/services/services';
+import { Footer } from './components/footer/footer';
+import { Faq } from "./components/faq/faq";
+import { Contact } from './components/contact/contact';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, Header,
+    About, Pricing, Testimonials,
+    Nav, Footer, Faq, Contact],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('FrenchClassWebsite');
+}
